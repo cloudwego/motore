3c8deee5ff9a7fc98f8ffaf183fe754a3559ed1f
Millione <isjieliu@163.com>
2022-11-17 19:45:21 +0800

    feat: add map_response

---
+++ motore/src/service/ext/map_response.rs
@@ -0,0 +1,45 @@
+use std::fmt;
+
+use futures::{Future, TryFutureExt};
+
+use crate::Service;
+/// Service returned by the [`map_response`] combinator.
+///
+/// [`map_response`]: crate::service::ext::ServiceExt::map_response
+#[derive(Clone)]
+pub struct MapResponse<S, F> {
+    pub(crate) inner: S,
+    pub(crate) f: F,
+}
+
+impl<S, F, Cx, Req, Response> Service<Cx, Req> for MapResponse<S, F>
+where
+    S: Service<Cx, Req>,
+    F: FnOnce(S::Response) -> Response + Clone + Send,
+{
+    type Response = Response;
+    type Error = S::Error;
+    type Future<'cx> = impl Future<Output = Result<Self::Response, Self::Error>> + Send + 'cx
+    where
+        Cx: 'cx,
+        Self: 'cx;
+
+    fn call<'cx, 's>(&'s self, cx: &'cx mut Cx, req: Req) -> Self::Future<'cx>
+    where
+        's: 'cx,
+    {
+        self.inner.call(cx, req).map_ok(self.f.clone())
+    }
+}
+
+impl<S, F> fmt::Debug for MapResponse<S, F>
+where
+    S: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MapResponse")
+            .field("inner", &self.inner)
+            .field("f", &format_args!("{}", std::any::type_name::<F>()))
+            .finish()
+    }
+}
--- motore/src/service/ext/mod.rs
+++ motore/src/service/ext/mod.rs
@@ -1,7 +1,8 @@
 use crate::Service;
 
 mod map_err;
-pub use self::map_err::MapErr;
+mod map_response;
+pub use self::{map_err::MapErr, map_response::MapResponse};
 
 /// An extension trait for `Service`s that provides a variety of convenient
 /// adapters
@@ -11,6 +12,17 @@ pub trait ServiceExt<Cx, Req>: Service<Cx, Req> + Sized {
     /// This method can be used to change the [`Error`] type of the service
     /// into a different type. It is similar to the [`Result::map_err`] method.
     fn map_err<E, F: FnOnce(Self::Error) -> E>(self, f: F) -> MapErr<Self, F>;
+
+    /// Maps this service's response value to a different value.
+    ///
+    /// This method can be used to change the [`Response`] type of the service
+    /// into a different type. It is similar to the [`Result::map`]
+    /// method. You can use this method to chain along a computation once the
+    /// service's response has been resolved.
+    fn map_response<F: FnOnce(Self::Response) -> Response, Response>(
+        self,
+        f: F,
+    ) -> MapResponse<Self, F>;
 }
 
 impl<T, Cx, Req> ServiceExt<Cx, Req> for T
@@ -19,5 +31,12 @@ where
 {
     fn map_err<E, F: FnOnce(Self::Error) -> E>(self, f: F) -> MapErr<Self, F> {
         MapErr { inner: self, f }
+    }
+
+    fn map_response<F: FnOnce(Self::Response) -> Response, Response>(
+        self,
+        f: F,
+    ) -> MapResponse<Self, F> {
+        MapResponse { inner: self, f }
     }
 }
